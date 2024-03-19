95ca35c8362b96684c26607aa61e8e808e959796
Cheikh Sidiya <cheikhsidiya55@gmail.com>
2024-03-19 16:18:06 +0000

    Added more websites

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,12 @@ import time
 import os
 
 uas_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "user_agents.txt"))
-website_to_keep_alive = "https://vote-app-6owd.onrender.com/"
+websites = {
+    "https://vote-app-6owd.onrender.com/": "https://api.render.com/deploy/srv-cns9ainjbltc739obg60?key=gOB8ywpivu8",
+    "https://vote-app-1.onrender.com": "https://api.render.com/deploy/srv-cnsrfhnjbltc73d5af8g?key=9pMU2rivPEw",
+    "https://vote-app-2.onrender.com": "https://api.render.com/deploy/srv-cnsrgrf79t8c73a84lh0?key=NRcYguwn4jQ",
+    "https://vote-app-3.onrender.com": "https://api.render.com/deploy/srv-cnsrkcgcmk4c73eqgha0?key=q0FCJqvS5Cw",
+}
 
 
 def save_uas(lst):
@@ -58,18 +63,20 @@ uas_list = load_uas()
 def send_request():
     # Replace with your target URL
     headers = {"User-Agent": random.choice(uas_list).strip()}
-    try:
-        res = requests.get(website_to_keep_alive, headers=headers)
-        if res.status_code == 200:
-            print("Request successful")
-        else:
-            print(res.text)
-            print(f"Request failed with status code {res.status_code}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    for website in websites:
+        try:
+            res = requests.get(website, headers=headers)
+            if res.status_code == 200:
+                print("Request successful")
+            else:
+                print(f"Request failed with status code {res.status_code}")
+                print(f"Triggering the auto deploy: ")
+                res = requests.get(websites[website])
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
     while True:
         send_request()
-        time.sleep(400)  # Sleep for 600 seconds (10 minutes)
+        time.sleep(500)
